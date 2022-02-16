16c9ae9f461362e53af2bf24df567434be399d98
HTAYHTAYHLAING\HtayHtayHlaing <scm.htayhtayhlaing@gmail.com>
2022-02-16 09:26:43 +0630

    [master]Initial Commit

---
+++ Csharp_Training/Csharp_Training/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Csharp_Training");
+            Console.ReadLine();
+        }
+    }
+}
