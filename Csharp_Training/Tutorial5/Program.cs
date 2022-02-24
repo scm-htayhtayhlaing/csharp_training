55a8091bc040f285c196da0e43fe9f52020547c1
HTAYHTAYHLAING\HtayHtayHlaing <scm.htayhtayhlaing@gmail.com>
2022-02-24 09:08:19 +0630

    test5

---
+++ Csharp_Training/Tutorial5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("T 5");
+            Console.ReadLine();
+        }
+    }
+}
